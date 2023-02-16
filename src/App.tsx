b0682bb2350320764537bf4ebc8c22643d50d7dc
Samson <sammossallam@gmail.com>
2023-02-15 22:11:02 -0500

    removing nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from "./components/Login/Login";
 
 import NavBar from "./components/NavBar/NavBar";
